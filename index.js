7c279ed394c8a695caaa461bb7b9919011b4f593
Malik <malik@Alephs-MacBook-Pro.local>
2020-03-31 15:54:33 +0800

    Movie-App: integrate redux with app's state

--- index.js
+++ index.js
@@ -2,8 +2,31 @@
  * @format
  */
 
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json';
 import HomeScreen from './src/screens/HomeScreen';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './src/redux/reducer/ReducerFactory';
+import createSagaMiddleware from 'redux-saga';
+import dataSaga from './src/redux/saga/SagaFactory';
 
-AppRegistry.registerComponent(appName, () => HomeScreen);
+const sagaMiddleware = createSagaMiddleware();
+
+function configureStore() {
+    const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+    sagaMiddleware.run(dataSaga);
+    return store;
+};
+
+const store = configureStore();
+
+const MovieApp = () => (
+    <Provider store={store}>
+      <HomeScreen />
+    </Provider>
+  );
+
+
+AppRegistry.registerComponent(appName, () => MovieApp);
