286a802c1150a59e170cfad37bfe03b5a42e4f30
Malik <malik@Alephs-MacBook-Pro.local>
2020-03-31 15:14:07 +0800

    Movie-App: create  Movie model structure

---
+++ src/model/MoviesModel/Movie.ts
@@ -0,0 +1,8 @@
+import CommonModel from '../CommonModel';
+
+export declare type Movies = Movie[];
+
+export declare type UTMovie = Movie | undefined;
+
+export default interface Movie extends CommonModel {
+};
