c1d2419442d771881d0f6a36ce59934aec2b23f3
Malik <malik@Alephs-MacBook-Pro.local>
2020-03-31 16:47:36 +0800

    Movie-App connect redux to home page

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
-
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import AppAction from '../../redux/action/AppAction';
 
 const HomeScreen = () => {
+    const movieData = useSelector(state => state);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(AppAction.getMovies());
+    }, []);
+
+    console.log('movieList', movieData);
+
     return <></>
 }
 
