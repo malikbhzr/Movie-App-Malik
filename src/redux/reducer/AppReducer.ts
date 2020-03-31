1b399ab008edefce3524cfff01c3b62c29af71c6
Malik <malik@Alephs-MacBook-Pro.local>
2020-03-31 15:32:08 +0800

    Movie-App: setup redux: add root and movie reducers

---
+++ src/redux/reducer/AppReducer.ts
@@ -0,0 +1,58 @@
+import AppState from '../state/AppState';
+import { iDataState } from '../state/IState';
+import IAction from '../action/IAction';
+import AppAction from '../action/AppAction';
+import  Movie  from '../../model/MoviesModel/Movie';
+import UtilAction from '../action/UtilAction';
+
+export default class AppReducer {
+  private static readonly _initialState: AppState = {
+    movies: [],
+    state: iDataState.initial,
+    errorMessage: '',
+  };
+
+  public static reducer(
+    state: AppState = AppReducer._initialState,
+    action: IAction<any, Movie>,
+  ): AppState {
+    switch (action.type) {
+      case AppAction.GET_MOVIES:
+        return {
+          ...state,
+          state: iDataState.loading,
+          errorMessage: '',
+        };
+
+      case AppAction.LOAD_MOVIES:
+        return {
+          ...state,
+          movies: action.data !== null ? action.data! : [],
+          state: iDataState.loaded,
+          errorMessage: '',
+        };
+
+      case AppAction.SHOW_MOVIE_LIST:
+        return {
+          ...state,
+          state: iDataState.loaded,
+        };
+
+      case AppAction.SHOW_MOVIE_DETAIL:
+        return {
+          ...state,
+          state: iDataState.loading,
+          errorMessage: '',
+        };
+
+      case UtilAction.ERROR:
+        return {
+          ...state,
+          state: iDataState.error,
+          errorMessage: action.error,
+        };
+      default:
+        return state;
+    }
+  }
+}
