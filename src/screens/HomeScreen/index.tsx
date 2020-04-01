c386668cab3fc41c59e838a98e9fcb402a33a79d
anudeepNarasappa <anudeep@aleph-labs.com>
2020-04-01 21:42:38 +0800

    [movie-app]:Implemented Splash screen and app icon

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,50 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import { ScrollView, SafeAreaView, View, ActivityIndicator, } from 'react-native';
-import { useSelector, useDispatch } from "react-redux";
+import React, {useEffect, useState} from 'react';
+import {ScrollView, SafeAreaView, View, ActivityIndicator} from 'react-native';
+import {useSelector, useDispatch} from 'react-redux';
 import AppAction from '../../redux/action/AppAction';
 import MovieCards from '../../components/MovieCards';
-import { Title } from './styled';
-import { SearchBar } from 'react-native-elements';
+import {Title} from './styled';
+import {SearchBar} from 'react-native-elements';
+import SplashScreen from 'react-native-splash-screen';
 
+const HomeScreen = ({navigation}: any) => {
+  const movieData = useSelector(state => state);
+  const dispatch = useDispatch();
+  const [searchMovie, setSearchMovie] = useState(null);
+  useEffect(() => {
+    dispatch(AppAction.getMovies());
+    SplashScreen.hide();
+  }, []);
 
-const HomeScreen = ({ navigation }: any) => {
-    const movieData = useSelector(state => state);
-    const dispatch = useDispatch();
-    const [ searchMovie, setSearchMovie ] = useState(null);
-    useEffect(() => {
-        dispatch(AppAction.getMovies());
-    }, []);
-
-
-    const showMovieList = () => {
-        const { navigate } = navigation;
-        if(movieData && movieData.appData){
-            const { Search } = movieData.appData.movies;
-            return (
-                <ScrollView>
-                    <Title marginTop={20}>Movies on Theatre</Title>
-                    <MovieCards nav={navigate} width={230} height={340} textWidth={230} movieList={Search} borderRadius={10} />
-                    <Title marginTop={40}>Coming Soon</Title>
-                    <View style={{ marginTop: 64}}>
-                        <MovieCards nav={navigate} width={104} height={154} textWidth={230} movieList={Search} borderRadius={6} />
-                    </View>
-                    <View style={{ marginTop: 80}}>
-                        <MovieCards nav={navigate} width={104} height={154}  movieList={Search} borderRadius={6} />
-                    </View>
-                </ScrollView>
-            )
-        }
-        return <ActivityIndicator />
+  const showMovieList = () => {
+    const {navigate} = navigation;
+    if (movieData && movieData.appData) {
+      const {Search} = movieData.appData.movies;
+      return (
+        <ScrollView>
+          <Title marginTop={20}>Movies on Theatre</Title>
+          <MovieCards
+            nav={navigate}
+            width={230}
+            height={340}
+            textWidth={230}
+            movieList={Search}
+            borderRadius={10}
+          />
+          <Title marginTop={40}>Coming Soon</Title>
+          <View style={{marginTop: 64}}>
+            <MovieCards
+              nav={navigate}
+              width={104}
+              height={154}
+              textWidth={230}
+              movieList={Search}
+              borderRadius={6}
+            />
+          </View>
+          <View style={{marginTop: 80}}>
+            <MovieCards
+              nav={navigate}
+              width={104}
+              height={154}
+              movieList={Search}
+              borderRadius={6}
+            />
+          </View>
+        </ScrollView>
+      );
     }
+    return <ActivityIndicator />;
+  };
 
-    return(
-        <SafeAreaView style={{ flex: 1, backgroundColor: '#1d1e28' }}>
-                <SearchBar
-                    placeholder="Type Here..."
-                />
-                {showMovieList()}
-        </SafeAreaView>
-    )
-}
+  return (
+    <SafeAreaView style={{flex: 1, backgroundColor: '#1d1e28'}}>
+      <SearchBar placeholder="Type Here..." />
+      {showMovieList()}
+    </SafeAreaView>
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
